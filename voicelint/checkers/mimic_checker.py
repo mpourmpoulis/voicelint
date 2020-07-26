2b3fc52877ee9ce511f40fba22ddc5fdcdfab409
mpourmpoulis <35875229+mpourmpoulis@users.noreply.github.com>
2020-07-26 23:32:55 +0300

    Better differentiate errors regarding mimic

--- voicelint/checkers/mimic_checker.py
+++ voicelint/checkers/mimic_checker.py
@@ -14,19 +14,32 @@ class MimicChecker(BaseChecker):
     priority = -1
     msgs = {
         'E01': (
-            'Returns a non-unique constant.',
+            'Mimic should accept single word strings separated by `,` eg Mimic("hello","world") vs Mimic(["hello","world"])',
             "mimic-separated-strings",
-            'All constants returned in a function should be unique.'
+            ""
         ),
+        'E02': (
+            'Strings in Mimic should consist of a single word,eg Mimic("hello","world") vs Mimic("hello world")',
+            "mimic-single-word-strings",
+            ""
+        ),
+        'E03': (
+        	"Mimic can only accept `extra` as keyword argument, in case you want to fetch the Mimi content from a spoken extra. Other names are going to raise an exception during runtime",
+        	"mimic-invalid-keyword",
+        	""
+        )
+
     }
 
 
     def visit_call(self,node):
-    	# print("data")
-    	# print(node.func.as_string())
-    	if node.func.as_string()=="Mimic":
-    		if not all(isinstance(x,astroid.nodes.Const) and x.pytype()=="builtins.str"
-    				and len(x.value.strip().split())==1 for x in node.args):
+        if node.func.as_string()=="Mimic":
+            if not all(isinstance(x,astroid.nodes.Const) and x.pytype()=="builtins.str" for x in node.args):
+               self.add_message("mimic-separated-strings", node=node)
+            else:
+                for s in node.args:
+                    if len(s.value.strip().split())!=1:
+                    	self.add_message("mimic-single-word-strings", node=s)
+            if node.keywords and any(x.arg != "extra" for x in node.keywords):
+            	self.add_message("mimic-invalid-keyword", node=node)
 
-    			self.add_message("mimic-separated-strings", node=node)
-    
