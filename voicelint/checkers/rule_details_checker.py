788f5169d0c8717a28495ca7c20caef7c620bcc6
mpourmpoulis <35875229+mpourmpoulis@users.noreply.github.com>
2020-07-27 16:42:03 +0300

    Check for ccrtype.global  When there are contexts

--- voicelint/checkers/rule_details_checker.py
+++ voicelint/checkers/rule_details_checker.py
@@ -22,6 +22,12 @@ class RuleDetailsChecker(BaseChecker):
             "mapping-rule-is-not-ccr",
             ""
         ),
+        'E06': (
+            "CCRType.GLOBAL cannot be used with contexts, whether application aka `executable`,`title`" + 
+            ",... or Function Contexts (`function_context`). You need to use `CCRType.APP` in both cases!",
+            "global-no-context",
+            ''
+        )
 
     }
 
@@ -32,18 +38,23 @@ class RuleDetailsChecker(BaseChecker):
         self.cl[node.name] = [x.as_string() for x in node.bases]
 
     def visit_return(self,node):
-        if isinstance(node.parent,astroid.FunctionDef) and node.parent.name=="get_rule":
-            if not node.is_tuple_return():   return 
-            name = node.value.getitem(astroid.Const(0)).as_string()
-            if name not in self.cl: return 
-            details = node.value.getitem(astroid.Const(1))
-            if not isinstance(details,astroid.Call): return 
-            try : 
-                ccrtype = next(x.value for x in details.keywords if x.arg == "ccrtype") 
-                if self.cl[name] == ["MappingRule"]:
-                    self.add_message("mapping-rule-is-not-ccr",node=node)
-            except StopIteration:
-                pass
+        if not isinstance(node.parent,astroid.FunctionDef) or node.parent.name!="get_rule":
+            return 
+        if not node.is_tuple_return():   return 
+        name = node.value.getitem(astroid.Const(0)).as_string()
+        if name not in self.cl: return 
+        details = node.value.getitem(astroid.Const(1))
+        if not isinstance(details,astroid.Call): return 
+        try : 
+            ccrtype = next(x.value for x in details.keywords if x.arg == "ccrtype") 
+            if self.cl[name] == ["MappingRule"]:
+                self.add_message("mapping-rule-is-not-ccr",node=node)
+            if ccrtype.as_string()=="CCRType.GLOBAL":
+                if any(x for x in details.keywords if x.arg in ["executable","title","handle","function_context"]):
+                    self.add_message("global-no-context",node=node)
+
+        except StopIteration:
+            pass
 
 
 
