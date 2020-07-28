d612099ec5d5f1e9ddb85ff7403fc6c407343030
mpourmpoulis <35875229+mpourmpoulis@users.noreply.github.com>
2020-07-28 15:23:10 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,17 +6,18 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="voicelint", # Replace with your own username
     version="0.0.0",
-    author="Example Author",
-    author_email="author@example.com",
-    description="A small example package",
+    author="mpourmpoulis",
+    # author_email="author@example.com",
+    description="pylint plug-in for users of the program and by voice toolkit Caster",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/mpourmpoulis/voicelint",
     packages=setuptools.find_packages(),
     classifiers=[
+        'Development Status :: 3 - Alpha',
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.0',
 )
