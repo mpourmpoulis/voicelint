58bec714fe2e3981683d04151f287f138c160990
mpourmpoulis <35875229+mpourmpoulis@users.noreply.github.com>
2020-08-09 21:08:25 +0300

    The checks for mimic can now generate an error messages dynamically allowing to show precise suggestions on how to correct them!

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,3 +3,9 @@
 ## Added
 
 - add check to catch errors with using multiple strings specs in Key,Text,Mouse action,e.g. `Key("c-a","c-c")` vs `Key("c-a,c-c")`
+
+
+## Changed
+
+- The checks for mimic can now generate an error messages dynamically allowing to show precise suggestions on how to correct them!
+
--- voicelint/checkers/mimic_checker.py
+++ voicelint/checkers/mimic_checker.py
@@ -6,6 +6,9 @@ from pylint.interfaces import IAstroidChecker
 
 from voicelint.utility import apply_id
 
+def is_string(x):
+    return isinstance(x,astroid.nodes.Const) and x.pytype()=="builtins.str"
+
 @apply_id
 class MimicChecker(BaseChecker):
     __implements__ = IAstroidChecker
@@ -14,17 +17,17 @@ class MimicChecker(BaseChecker):
     priority = -1
     msgs = {
         'E01': (
-            'Mimic should accept single word strings separated by `,` eg Mimic("hello","world") vs Mimic(["hello","world"])',
+            'Mimic should accept single word strings separated by `,` eg %s',
             "mimic-separated-strings",
             ""
         ),
         'E02': (
-            'Strings in Mimic should consist of a single word,eg Mimic("hello","world") vs Mimic("hello world")',
+            'Strings in Mimic should consist of a single word,instead of `%s` you should use `%s`', 
             "mimic-single-word-strings",
             ""
         ),
         'E03': (
-        	"Mimic can only accept `extra` as keyword argument, in case you want to fetch the Mimi content from a spoken extra. Other names are going to raise an exception during runtime",
+        	"Mimic can only accept `extra` as keyword argument, in case you want to fetch the Mimic content from a spoken extra. Other names are going to raise an exception during runtime",
         	"mimic-invalid-keyword",
         	""
         )
@@ -34,12 +37,23 @@ class MimicChecker(BaseChecker):
 
     def visit_call(self,node):
         if node.func.as_string()=="Mimic":
-            if not all(isinstance(x,astroid.nodes.Const) and x.pytype()=="builtins.str" for x in node.args):
-               self.add_message("mimic-separated-strings", node=node)
+            if not all(is_string(x) for x in node.args):
+                args = 'Mimic("hello","world") vs Mimic(["hello","world"])'
+                if node.args and isinstance(node.args[0],(astroid.List,astroid.Tuple)):
+                    elements = node.args[0].elts
+                    temporary = []
+                    for e in elements:
+                        if is_string(e) and len(e.value.strip().split())!=1:
+                            temporary.append(",".join('"%s"'%x for x in e.value.strip().split()))
+                        else:
+                            temporary.append(e.as_string())
+                    args = 'Mimic(%s)' % ','.join(temporary)
+                self.add_message("mimic-separated-strings", node=node,args=args)
             else:
                 for s in node.args:
                     if len(s.value.strip().split())!=1:
-                    	self.add_message("mimic-single-word-strings", node=s)
+                        text = ",".join('"%s"'%x for x in s.value.strip().split())
+                        self.add_message("mimic-single-word-strings", node=s,args=(s.as_string(),text))
             if node.keywords and any(x.arg != "extra" for x in node.keywords):
             	self.add_message("mimic-invalid-keyword", node=node)
 
